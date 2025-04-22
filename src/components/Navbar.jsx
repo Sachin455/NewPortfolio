6c5b525afcc4219d9f5f8fcdf538aff89e5ced05
sachin455 <karkisachin455@gmail.com>
2025-04-23 00:06:11 +0545

    added links to icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,16 +11,24 @@ export const Navbar = () => {
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl px-20">
                 <motion.div whileHover={{scale:2.2}} whileTap={{scale: 0.8}}>
+                    <a href="https://www.facebook.com/sachin.karki.3344">
                     <FaFacebook />
+                    </a>
                 </motion.div>
                 <motion.div whileHover={{scale:2.2}} whileTap={{scale: 0.8}}>
+                    <a href="https://github.com/Sachin455">
                     <FaGithub />
+                    </a>
                 </motion.div>
                 <motion.div whileHover={{scale:2.2}} whileTap={{scale: 0.8}}>
+                    <a href="https://www.instagram.com/sachinkarki_19/">
                     <FaInstagram />
+                    </a>
                 </motion.div>
                 <motion.div whileHover={{scale:2.2}} whileTap={{scale: 0.8}}>
+                    <a href="https://www.linkedin.com/in/sachin-karki-772369159/">
                     <FaLinkedin />
+                    </a>
                 </motion.div>
 
 
